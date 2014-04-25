bfd136dd2d2246c6e4e562bec14480d4e021a0ff
bhargav <bhargav@bhargav-laptop>
2014-04-24 19:42:52 -0700

    bugfix

--- Timeline_project.php
+++ Timeline_project.php
@@ -1,7 +1,7 @@
 <?php
 include './tasks.php';
 include './Template.php';
-$head='<script src="/api/timeline-api.js?bundle=true" type="text/javascript"></script>
+$head = '<script src="/api/timeline-api.js?bundle=true" type="text/javascript"></script>
 
         <link rel="stylesheet" href="/css/styles.css" type="text/css" /> <!-- load your css after Timelines -->
         <script src="js/timeline_p.js" type="text/javascript"></script>
@@ -16,18 +16,50 @@ bottom:5px !important;
 }
         </style>';
 
-$header = new Template('./header.php',array("head"=>$head,"current_page"=>1,"body"=>'onload="onLoad();" onresize="onResize();"'));
+$header = new Template('./header.php', array("head" => $head, "current_page" => 1, "body" => 'onload="onLoad();" onresize="onResize();"'));
 $header->out();
-if($_SESSION['access_level'] == 1){
+if ($_SESSION['access_level'] == 1) {
     header('Location: /Timeline.php');
 }
-
 ?>
-        <div id="body">
-            <div id="tl" class="timeline-default" style="float: left;width: 100%;position: absolute;left: 0px;top: 70px;margin-bottom:80px;">
-            </div>
-        </div>
+<script>
+    Timeline.DefaultEventSource.Event.prototype.fillTime = function(elmt,
+            labeller) {
+        if (this._instant) {
+            if (this.isImprecise()) {
+
+                elmt.appendChild(elmt.ownerDocument.createTextNode(this.getProperty("start")));
+                elmt.appendChild(elmt.ownerDocument.createElement("br"));
+
+                elmt.appendChild(elmt.ownerDocument.createTextNode(this.getProperty("end")));
+            } else {
+
+                elmt.appendChild(elmt.ownerDocument.createTextNode(this.getProperty("start")));
+            }
+        } else {
+            if (this.isImprecise()) {
+                elmt.appendChild(elmt.ownerDocument.createTextNode(
+                        this.getProperty("start") + " ~ " +
+                        this.getProperty("latestStart")));
+                elmt.appendChild(elmt.ownerDocument.createElement("br"));
+                elmt.appendChild(elmt.ownerDocument.createTextNode(
+                        this.getProperty("earliestEnd") + " ~ " +
+                        this.getProperty("end")));
+            } else {
+
+                elmt.appendChild(elmt.ownerDocument.createTextNode(this.getProperty("start")));
+                elmt.appendChild(elmt.ownerDocument.createElement("br"));
+
+                elmt.appendChild(elmt.ownerDocument.createTextNode(this.getProperty("end")));
+            }
+        }
+    };
+</script>
+<div id="body">
+    <div id="tl" class="timeline-default" style="float: left;width: 100%;position: absolute;left: 0px;top: 70px;margin-bottom:80px;">
+    </div>
+</div>
 <input type="hidden" id="project_uid" value="2"/>
-<script>$("#tl").height($(document).height()-70);</script>
+<script>$("#tl").height($(document).height() - 70);</script>
 </body>
 </html>
