05fdf32c76de3fce1e4c61bcdb2e9c412ffb206f
bhargav <bhargav@bhargav-laptop>
2014-05-05 19:40:00 -0700

    final commit

--- functions.php
+++ functions.php
@@ -154,7 +154,7 @@ if ($_SESSION['userLoggedin']) {
        
 $count = 0;
 if($_SESSION['access_level'] == 2){
-    $sqlquery = "select t1.uid,t1.name,t1.description,t1.project_uid,t2.name as project_name,t1.start_date,t1.est_end_date,t1.end_date,t1.status,t1.priority,t1.deleted from tasks t1, project t2 where t2.uid=t1.project_uid and t1.project_uid=".$_GET['project'] ;
+    $sqlquery = "select t1.uid,t1.name,t1.description,t1.project_uid,t2.name as project_name,t1.start_date,t1.est_end_date,t1.end_date,t1.status,t1.priority from tasks t1, project t2 where t2.uid=t1.project_uid and t1.project_uid=".$_GET['project'] ;
 }else{
     $sqlquery = "select * from tasks where uid in(select task_uid from user_tasks where user_uid=".$_SESSION['user_uid'].")" ;
 }
